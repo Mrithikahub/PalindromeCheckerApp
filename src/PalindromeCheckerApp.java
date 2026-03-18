d3600c5cc84a457d5e7e7818a0d358b9a0076e2f
Mrithikahub <mrithikakumar1112@gmail.com>
2026-03-18 23:28:54 +0530

    UC10: Ignore case palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,19 +1,15 @@
 public class PalindromeCheckerApp {
-
-    static boolean isPalindrome(String str, int left, int right) {
-        if(left >= right) return true;
-
-        if(str.charAt(left) != str.charAt(right))
-            return false;
-
-        return isPalindrome(str, left + 1, right - 1);
-    }
-
     public static void main(String[] args) {
 
-        String str = "madam";
+        String str = "Madam";
 
-        if(isPalindrome(str, 0, str.length() - 1))
+        // convert to lowercase
+        str = str.toLowerCase();
+
+        // reverse
+        String rev = new StringBuilder(str).reverse().toString();
+
+        if(str.equals(rev))
             System.out.println("Palindrome");
         else
             System.out.println("Not Palindrome");
