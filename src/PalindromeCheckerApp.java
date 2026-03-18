6a45719ac7364e50afb88ca947fe80a00319aeb9
Mrithikahub <mrithikakumar1112@gmail.com>
2026-03-18 23:37:04 +0530

    UC13: Performance comparison

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,22 +1,20 @@
-interface PalindromeStrategy {
-    boolean check(String str);
-}
-
-class ReverseStrategy implements PalindromeStrategy {
-    public boolean check(String str) {
-        String rev = new StringBuilder(str).reverse().toString();
-        return str.equals(rev);
-    }
-}
-
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
-        PalindromeStrategy strategy = new ReverseStrategy();
+        String str = "madam";
 
-        if(strategy.check("madam"))
+        long start = System.nanoTime();
+
+        String rev = new StringBuilder(str).reverse().toString();
+        boolean result = str.equals(rev);
+
+        long end = System.nanoTime();
+
+        if(result)
             System.out.println("Palindrome");
         else
             System.out.println("Not Palindrome");
+
+        System.out.println("Time taken: " + (end - start) + " ns");
     }
 }
