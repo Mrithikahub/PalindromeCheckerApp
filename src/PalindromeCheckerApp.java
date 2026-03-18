a0ae0a6f584ef872d952833e2cb7b9222ed6568a
Mrithikahub <mrithikakumar1112@gmail.com>
2026-03-18 23:33:02 +0530

    UC11: OOP palindrome service

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,15 +1,17 @@
+class PalindromeService {
+
+    boolean isPalindrome(String str) {
+        String rev = new StringBuilder(str).reverse().toString();
+        return str.equals(rev);
+    }
+}
+
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
-        String str = "Madam";
+        PalindromeService service = new PalindromeService();
 
-        // convert to lowercase
-        str = str.toLowerCase();
-
-        // reverse
-        String rev = new StringBuilder(str).reverse().toString();
-
-        if(str.equals(rev))
+        if(service.isPalindrome("madam"))
             System.out.println("Palindrome");
         else
             System.out.println("Not Palindrome");
